a9388788452dd5801f61e0347b1b4af3090618cb
Muhammed Yikit <mhmmtykt76@gmail.com>
2023-07-31 00:54:18 +0300

    main page

--- App.js
+++ App.js
@@ -1,15 +1,109 @@
 // Home.js
 
-import React from 'react';
-import { View, Text } from 'react-native';
-import styles from './Style';  // style.js dosyasını burada import ediyoruz.
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import styles from "./Style"; // style.js dosyasını burada import ediyoruz.
 
 export default function Home() {
+  const [threds, setThreds] = useState([
+    {
+      id: 1,
+      name: "Lorem Insup",
+      date: "20.12.2023 20:12",
+    },
+    {
+      id: 1,
+      name: "Lorem Insup",
+      date: "20.12.2023 20:12",
+    },
+    {
+      id: 1,
+      name: "Lorem Insup",
+      date: "20.12.2023 20:12",
+    },
+    {
+      id: 1,
+      name: "Lorem Insup",
+      date: "20.12.2023 20:12",
+    },
+    {
+      id: 1,
+      name: "Lorem Insup",
+      date: "20.12.2023 20:12",
+    },
+    {
+      id: 1,
+      name: "Lorem Insup",
+      date: "20.12.2023 20:12",
+    },
+    {
+      id: 1,
+      name: "Lorem Insup",
+      date: "20.12.2023 20:12",
+    },
+    {
+      id: 1,
+      name: "Lorem Insup",
+      date: "20.12.2023 20:12",
+    },
+    {
+      id: 1,
+      name: "Lorem Insup",
+      date: "20.12.2023 20:12",
+    },
+    {
+      id: 1,
+      name: "Lorem Insup",
+      date: "20.12.2023 20:12",
+    },
+    {
+      id: 1,
+      name: "Lorem Insup",
+      date: "20.12.2023 20:12",
+    },
+    {
+      id: 1,
+      name: "Lorem Insup",
+      date: "20.12.2023 20:12",
+    },
+  ]);
+
   return (
     <View style={styles.container}>
       <View style={styles.aiBar}>
-        <Text style={styles.aiBarText}>AI CHAT</Text>
-        <Text style={styles.versionText}>V1.0.0</Text>
+        <View>
+          <Text style={styles.aiBarText}>Chat AI</Text>
+        </View>
+        <View>
+          <Text style={styles.versionText}>V1.0.0</Text>
+        </View>
+      </View>
+      <View style={styles.main}>
+        <View style={styles.headBar}>
+          <TouchableOpacity style={styles.newChat}>
+            <Text style={styles.newChatText}>Yeni Chat</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.chatContainer}>
+          <ScrollView>
+            {threds.map((thred) => {
+              return (
+                <View style={styles.chatItem} key={thred.id}>
+                  <View>
+                    <Text style={styles.chatItemTitle}>{thred.name}</Text>
+                    <Text style={styles.chatItemDate}>{thred.date}</Text>
+                  </View>
+                  <View>
+                    <Text>Delete</Text>
+                  </View>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+        <View style={styles.desingedBy}>
+          <Text style={styles.desingedByText}>Desinged By Soulfly and Leora</Text>
+        </View>
       </View>
     </View>
   );
--- Style.js
+++ Style.js
@@ -1,30 +1,77 @@
 // style.js
 
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start', // ekranın üst kısmına kutuyu yerleştirmek için
-    backgroundColor: '#171D26',
-    padding: 10,
+    justifyContent: "flex-start", // ekranın üst kısmına kutuyu yerleştirmek için
+    backgroundColor: "#1E2530",
   },
   aiBar: {
-    width: 326,
-    height: 54,
-    backgroundColor: '#29323c', // açık renk
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    width: "100%",
+    backgroundColor: "#29323c", // açık renk
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 20,
+    padding: 20,
   },
   aiBarText: {
-    color: '#FFFFFF',
-    fontSize: 18,
+    color: "#FFFFFF",
+    fontSize: 25,
   },
   versionText: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     fontSize: 14,
-    textAlign: 'right',
+  },
+  headBar: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  newChat: {
+    backgroundColor: "#29323c",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 4,
+  },
+  newChatText: {
+    color: "white",
+  },
+  main: {
+    paddingHorizontal: 20,
+  },
+  chatContainer: {
+    height: "80%",
+    marginTop: 5,
+  },
+  chatItem: {
+    backgroundColor: "#29323c", // açık renk
+    borderRadius: 4,
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 5,
+  },
+  chatItemTitle: {
+    fontSize: 18,
+    color: "white",
+  },
+  chatItemDate: {
+    fontSize: 12,
+    color: "gray",
+  },
+  desingedBy: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 5,
+    height: "5%",
+  },
+  desingedByText: {
+    color: "white",
   },
 });
 
